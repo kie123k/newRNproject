1e56622887d68d76b6514416d83febc3f862a5a2
kie123k <kie123k@gmail.com>
2023-01-20 15:56:39 +0900

    230120

--- comp/MainPage/Feed/Tag.js
+++ comp/MainPage/Feed/Tag.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import { Component } from 'react/cjs/react.production.min';
 import styles from '../../../styles/styles';
 
-function Tag(tag){
-  return (
-    <View
-      style={{
-        position: 'absolute',
-        left: tag.x,
-        top: tag.y,
-        width: 50,
-        height: 20,
-        backgroundColor: 'black',
-        opacity: 1,
-      }}>
-      <Text style={{textAlign: 'center', textAlignVertical: 'center'}}>
-        {tag.tag_user}
-      </Text>
-    </View>
-  );
-}
 
+class Tag extends Component{
+  render() {
+    const {tag} = this.props;
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          left: tag.x,
+          top: tag.y,
+          width: 50,
+          height: 20,
+          backgroundColor: 'black',
+          opacity: 1,
+        }}>
+        <Text style={{textAlign: 'center', textAlignVertical: 'center'}}>
+          {tag.tag_user}
+        </Text>
+      </View>
+    );
+  }
+}
 export default Tag;
