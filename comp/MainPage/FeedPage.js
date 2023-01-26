a50c04fced1cf731550e8af55abfde79c955533d
rks <kie123k@gmail.com>
2023-01-27 07:13:32 +0900

    0127home

--- comp/MainPage/FeedPage.js
+++ comp/MainPage/FeedPage.js
@@ -16,56 +16,56 @@ import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs
 import MsgPage from './MsgPage';
 
 
+const botTab = createBottomTabNavigator();
+const stack = createMaterialTopTabNavigator();
+const Home = () => (
+  <>
+    <TopFrame />
+    <FeedList />
+  </>
+)
+const Reel = () => (
+  <></>
+)
+const Search = () => (
+  <></>
+)
+const Shop = () => (
+  <></>
+)
+const Info = () => (
+  <></>
+)
+const Tabs = () => (
+  <SafeAreaView style={styles.container}>
+    <botTab.Navigator>
+      <botTab.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}
+      />
+      <botTab.Screen
+        name="Search"
+        component={Search}
+        options={{headerShown: false}}/>
+      <botTab.Screen name="Reel" component={Reel} />
+      <botTab.Screen name="Shop" component={Shop} />
+      <botTab.Screen
+        name="Info"
+        component={Info}
+        options={{headerShown: false}}/>
+    </botTab.Navigator>
+  </SafeAreaView>
+)
 const Feedpage = () => {
-  const botTab = createBottomTabNavigator();
-  const stack = createMaterialTopTabNavigator();
-  const Home = () => (
-    <>
-      <TopFrame />
-      <FeedList />
-    </>
-  )
-  const Reel = () => (
-    <></>
-  )
-  const Search = () => (
-    <></>
-  )
-  const Shop = () => (
-    <></>
-  )
-  const Info = () => (
-    <></>
-  )
-  const Tabs = () => (
-    <SafeAreaView style={styles.container}>
-      <botTab.Navigator>
-        <botTab.Screen
-          name="Home"
-          component={Home}
-          options={{headerShown: false}}
-        />
-        <botTab.Screen
-          name="Search"
-          component={Search}
-          options={{headerShown: false}}/>
-        <botTab.Screen name="Reel" component={Reel} />
-        <botTab.Screen name="Shop" component={Shop} />
-        <botTab.Screen
-          name="Info"
-          component={Info}
-          options={{headerShown: false}}/>
-      </botTab.Navigator>
-    </SafeAreaView>
-  )
   return (
     <SafeAreaView style={styles.container}>
       <stack.Navigator tabBar={() => null}>
-        {/* <stack.Screen
-          name="Tabs"
+        <stack.Screen
+          name="bTabs"
           component={Tabs}
           options={{headerShown: false}}
-        /> */}
+        />
         <stack.Screen name="Msg" component={MsgPage} />
       </stack.Navigator>
     </SafeAreaView>
